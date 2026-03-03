efabd6325d0f1737d0498f1a9c0e8ad4d0018961
ADASurf <mahmudul.sikdar@ada.ac.uk>
2026-03-03 15:42:38 +0000

    queries3 done

--- src/queries/3_equipment_management.sql
+++ src/queries/3_equipment_management.sql
@@ -2,10 +2,18 @@
 .mode column
 
 -- 3.1 
+SELECT equipment_id, name, next_maintenance_date
+FROM equipment
+WHERE next_maintenance_date BETWEEN '2025-01-01' AND date('2025-01-01', '+30 days');
 
 
 -- 3.2 
-
+SELECT type AS equipment_type, COUNT(*) AS count
+FROM equipment
+GROUP BY type;
 
 -- 3.3 
-
+SELECT type AS equipment_type,
+       AVG(julianday('2025-01-01') - julianday(purchase_date)) AS avg_age_days
+FROM equipment
+GROUP BY type;
